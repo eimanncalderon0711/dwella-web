23aff3543999a7670d6b88dc8ecd68d181a24179
eimanncalderon0711 <calderon.eimann0711@gmail.com>
2025-06-13 18:02:21 +0800

    Update changes of sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,10 +6,12 @@ import Tab from "react-bootstrap/Tab";
 import { FaCircleInfo } from "react-icons/fa6";
 import { MdDashboard } from "react-icons/md";
 import { AiOutlineLineChart } from "react-icons/ai";
-import { CiMenuFries } from "react-icons/ci";
+import { RiMenu5Fill } from "react-icons/ri";
 import { TbUserQuestion } from "react-icons/tb";
 import "./side-bar.css"
 import { useState } from "react";
+import logo from "../assets/dwella-logo.png"
+import Dashboard from "../pages/resident/Dashboard";
 
 function SideBar() {
 
@@ -17,54 +19,62 @@ function SideBar() {
 
 
   return (
-    <aside className="vh-100 vw-100 d-flex overflow-hidden">
-      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-      <div className={`shadow side-bar ${isMenuOpen ? "side-active" : ""}`}>
-        <Nav variant="pills" className="flex-column">
-            <div className="d-flex justify-content-evenly w-100 d-block d-sm-none align-items-center">
-              <CiMenuFries  size={25} onClick={() => setIsMenuOpen(!isMenuOpen)}/>
-              <h3 className={`text-center mt-2 ${isMenuOpen ? "d-block": " d-none"}`}>Dwella</h3>
+    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+        <div className="d-flex vh-100 overflow-hidden">
+          <Nav variant="pills" className="flex-column gap-3 shadow show-sidebar">
+
+            <div className={`show-sidebar-menu ${isMenuOpen ? "text-start":"text-center"} d-block d-md-none`}>
+              <RiMenu5Fill onClick={() => setIsMenuOpen(!isMenuOpen)}/>
             </div>
-            <h3 className="text-center d-none d-sm-block mt-5 mb-5">Dwella</h3>
-            <Nav.Item className={`${isMenuOpen ? "nav-item-active" : ""}`}>
-              <Nav.Link eventKey="first" className='d-flex align-items-center gap-3'>
-                <MdDashboard size={25}/>
-                <span className={`navs-links fw-bold ${isMenuOpen ? "span-active" : ""}`}>Dashboard</span>
+
+            <a href="#" className={`my-5 fw-bold ${isMenuOpen ? "d-block" : "d-none "} d-md-block fs-2`}>
+              <img height={30} src={logo} alt="" />
+            </a>
+
+            <Nav.Item className="w-100">
+              <Nav.Link eventKey="first" className="d-flex align-items-center justify-content-start gap-2">
+                <MdDashboard />
+                <span className={`${ isMenuOpen ? "d-block" : "d-none"} d-md-block`}>
+                 Dashboard
+                </span>
               </Nav.Link>
             </Nav.Item>
-            <Nav.Item className={`${isMenuOpen ? "nav-item-active" : ""}`}>
-              <Nav.Link eventKey="second" className='d-flex align-items-center gap-3'>
-                <AiOutlineLineChart size={25}/>
-                <span className={`navs-links fw-bold ${isMenuOpen ? "span-active" : ""}`}>Financials</span>
+            
+            <Nav.Item className="w-100">
+              <Nav.Link eventKey="second" className="d-flex align-items-center justify-content-start gap-2">
+                <AiOutlineLineChart />
+                <span className={`${ isMenuOpen ? "d-block" : "d-none"} d-md-block`}>
+                 Financial
+                </span>
               </Nav.Link>
             </Nav.Item>
-            <Nav.Item className={`${isMenuOpen ? "nav-item-active" : ""}`}>
-              <Nav.Link eventKey="third" className='d-flex align-items-center gap-3'>
-                <FaCircleInfo size={25}/> 
-                <span className={`navs-links fw-bold ${isMenuOpen ? "span-active" : ""}`}>Notices</span>
-            </Nav.Link>
-            </Nav.Item>
-            <Nav.Item className={`${isMenuOpen ? "nav-item-active" : ""}`}>
-              <Nav.Link eventKey="fourth" className='d-flex align-items-center gap-3'>
-               <TbUserQuestion size={25}/>
-                <span className={`navs-links fw-bold ${isMenuOpen ? "span-active" : ""}`}>Inquiries</span>
+
+            <Nav.Item className="w-100">
+              <Nav.Link eventKey="third" className="d-flex align-items-center justify-content-start gap-2">
+                <FaCircleInfo />
+                <span className={`${ isMenuOpen ? "d-block" : "d-none"} d-md-block`}>
+                 Notices
+                </span>
               </Nav.Link>
             </Nav.Item>
-        </Nav>
-      </div>
-      <div className="w-100 main">
-          <Tab.Content>
-            <Tab.Pane eventKey="first">
-                <h1>Dashboard</h1>
-                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quos eum quasi! Tenetur corrupti molestias quibusdam id non eaque minima voluptates quas dolorem deserunt, architecto ullam itaque, aperiam hic alias quis quo ea totam quaerat! Hic porro, nam tempore placeat, doloribus, excepturi atque reprehenderit quis temporibus neque perferendis amet possimus recusandae voluptatum! Voluptatibus odit omnis voluptatum distinctio, nihil eaque. Nostrum dolor laudantium enim dolore doloremque tempora, ad facilis possimus animi maxime pariatur obcaecati tempore iure quod nemo quasi blanditiis vitae repellendus distinctio. Earum molestiae cumque voluptatem magnam, laboriosam explicabo harum fuga quisquam alias nostrum tempora iure. Magni sed consequuntur maiores?</div>
-            </Tab.Pane>
-            <Tab.Pane eventKey="second"><h1>Financials</h1></Tab.Pane>
-            <Tab.Pane eventKey="third"><h1>Notices</h1></Tab.Pane>
-            <Tab.Pane eventKey="fourth"><h1>Inquiries</h1></Tab.Pane>
+
+            <Nav.Item className="w-100">
+              <Nav.Link eventKey="fourth" className="d-flex align-items-center justify-content-start gap-2">
+                <TbUserQuestion />
+                <span className={`${ isMenuOpen ? "d-block" : "d-none"} d-md-block`}>
+                 Inquiries
+                </span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Tab.Content className="flex-grow-1">
+            <Tab.Pane eventKey="first"><Dashboard/></Tab.Pane>
+            <Tab.Pane eventKey="second">Financials</Tab.Pane>
+            <Tab.Pane eventKey="third">Notices</Tab.Pane>
+            <Tab.Pane eventKey="fourth">Inquiries</Tab.Pane>
           </Tab.Content>
-      </div>
-      </Tab.Container>
-    </aside>
+        </div>
+    </Tab.Container>
   );
 }
 
--- src/pages/resident/Dashboard.tsx
+++ src/pages/resident/Dashboard.tsx
@@ -1,7 +1,20 @@
+import { Button, Col, Container, Row, Stack } from "react-bootstrap"
+import  logo from "../../assets/_ 1.png"
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <Stack className="mt-5 flex-grow-1">
+      <Container>
+        <Row className="gap-3">
+            <Col className="gap-5 d-flex border bg-warning">
+                
+            </Col>
+            <Col className="border">
+              <div className="h-75 w-25"><img src={logo} alt="" /></div>
+            </Col>
+        </Row>
+      </Container>
+    </Stack>
   )
 }
 
