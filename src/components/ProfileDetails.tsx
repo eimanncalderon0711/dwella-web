92d34e79843f482984e71b5b62a3a956a871fc60
eimanncalderon0711 <calderon.eimann0711@gmail.com>
2025-06-20 12:20:45 +0800

    Edited font bolder for edit button

--- src/components/ProfileDetails.tsx
+++ src/components/ProfileDetails.tsx
@@ -9,9 +9,9 @@ function ProfileDetails() {
         <Row>
             <Col className="d-flex justify-content-between align-items-center">
                 <div className="fs-4 fw-bold">Details</div>
-                <div className="bg-primary px-3 py-2 text-light d-flex gap-2 align-items-center rounded-3">
-                    <span>Edit</span>
-                    <MdArrowOutward />
+                <div className="bg-primary px-3 py-2 text-light d-flex gap-2 align-items-center rounded-3" style={{cursor:"pointer"}}>
+                    <span className="fw-bold">Edit</span>
+                    <MdArrowOutward size={25}/>
                 </div>
             </Col>
         </Row>
