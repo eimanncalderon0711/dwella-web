b9559790ae93dcc0e14caf4e1ce3f65056b8126c
eimanncalderon0711 <calderon.eimann0711@gmail.com>
2025-07-20 18:08:42 +0800

    Add a field move_in_date to User model interface

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -7,6 +7,7 @@ export interface User {
   middle_name?: string;
   profile?: string | null;
   is_staff?: boolean;
+  move_in_date?: string;
   account_status?: 'inactive' | 'active'; // update with all possible statuses
   date_joined?: string; // ISO string format
   phone_number?: string;
